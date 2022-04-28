e858e18e4001cfeecae6b392c15572b87bd3ff01
gabfonseca <gfonseca.araujo05@gmail.com>
2022-04-28 17:03:14 -0300

    atualizar site primeira vez

---
+++ index.js
@@ -0,0 +1,6 @@
+function showModal() {
+	document.getElementById('conteinerbar').classList.toggle('active');
+	document.getElementById('modal').classList.toggle('active');
+	document.querySelector('body').classList.toggle('modal');
+};
+
